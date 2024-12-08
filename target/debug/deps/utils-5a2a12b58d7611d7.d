a436605ae5706c077a6e31361f7416865b59be95
The-Infinity-Glitch <theinfinityglitch@gmail.com>
2024-12-08 13:35:01 -0300

    Removed target

--- target/debug/deps/utils-5a2a12b58d7611d7.d
+++ target/debug/deps/utils-5a2a12b58d7611d7.d
@@ -1,10 +1,11 @@
-/home/themaster/Documentos/coding/rust/srzs-lang-work/target/debug/deps/libutils-5a2a12b58d7611d7.rmeta: srzs-lang/utils/src/lib.rs srzs-lang/utils/src/frontend/mod.rs srzs-lang/utils/src/frontend/lexer.rs srzs-lang/utils/src/handlers/mod.rs srzs-lang/utils/src/handlers/error_handler/mod.rs srzs-lang/utils/src/handlers/message_handler/mod.rs srzs-lang/utils/src/handlers/warning_handler/mod.rs srzs-lang/utils/src/types/mod.rs srzs-lang/utils/src/types/tokens.rs
+/home/themaster/Documentos/coding/rust/srzs-lang-work/target/debug/deps/libutils-5a2a12b58d7611d7.rmeta: srzs-lang/utils/src/lib.rs srzs-lang/utils/src/frontend/mod.rs srzs-lang/utils/src/frontend/lexer.rs srzs-lang/utils/src/frontend/parser.rs srzs-lang/utils/src/handlers/mod.rs srzs-lang/utils/src/handlers/error_handler/mod.rs srzs-lang/utils/src/handlers/message_handler/mod.rs srzs-lang/utils/src/handlers/warning_handler/mod.rs srzs-lang/utils/src/types/mod.rs srzs-lang/utils/src/types/tokens.rs
 
-/home/themaster/Documentos/coding/rust/srzs-lang-work/target/debug/deps/utils-5a2a12b58d7611d7.d: srzs-lang/utils/src/lib.rs srzs-lang/utils/src/frontend/mod.rs srzs-lang/utils/src/frontend/lexer.rs srzs-lang/utils/src/handlers/mod.rs srzs-lang/utils/src/handlers/error_handler/mod.rs srzs-lang/utils/src/handlers/message_handler/mod.rs srzs-lang/utils/src/handlers/warning_handler/mod.rs srzs-lang/utils/src/types/mod.rs srzs-lang/utils/src/types/tokens.rs
+/home/themaster/Documentos/coding/rust/srzs-lang-work/target/debug/deps/utils-5a2a12b58d7611d7.d: srzs-lang/utils/src/lib.rs srzs-lang/utils/src/frontend/mod.rs srzs-lang/utils/src/frontend/lexer.rs srzs-lang/utils/src/frontend/parser.rs srzs-lang/utils/src/handlers/mod.rs srzs-lang/utils/src/handlers/error_handler/mod.rs srzs-lang/utils/src/handlers/message_handler/mod.rs srzs-lang/utils/src/handlers/warning_handler/mod.rs srzs-lang/utils/src/types/mod.rs srzs-lang/utils/src/types/tokens.rs
 
 srzs-lang/utils/src/lib.rs:
 srzs-lang/utils/src/frontend/mod.rs:
 srzs-lang/utils/src/frontend/lexer.rs:
+srzs-lang/utils/src/frontend/parser.rs:
 srzs-lang/utils/src/handlers/mod.rs:
 srzs-lang/utils/src/handlers/error_handler/mod.rs:
 srzs-lang/utils/src/handlers/message_handler/mod.rs:
